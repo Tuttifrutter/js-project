edb4a93b4fb0a29a7d746c064547c38746d62751
Dmitry <sapega.99di@gmail.com>
2022-04-20 18:02:14 +0300

    Changed posts order to reverse

--- online-store-full-course-master/client/src/pages/Favourite.js
+++ online-store-full-course-master/client/src/pages/Favourite.js
@@ -16,7 +16,7 @@ const Favourite = observer(() => {
         let imgList=[];
         image.clearImages();
         getLikeList(userId).then(data =>{
-            for(let i=0; i<data.length; i++){
+            for(let i=data.length-1; i>=0; i--){
                 fetchOneImage(data[i]).then(result => {image.addImage(result)})
             }
             
